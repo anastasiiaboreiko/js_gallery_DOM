09ce7651ad2788c86733cade2a1bca90ab1e1a89
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-05-02 10:56:56 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const images = document.querySelector("#thumbs");
+const largeImg = document.querySelector("#largeImg");
+images.addEventListener("click", (even)=>{
+    even.preventDefault();
+    const link = even.target.closest("a");
+    if (!link || !images.contains(link)) return;
+    const largeImgSrc = link.getAttribute("href");
+    largeImg.src = new URL(largeImgSrc, location.origin).href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
